0b84eeadcdd65d62fc32688cd7f98c7f5ebf0fb2
catalinatan <zcemcat@ucl.ac.uk>
2025-01-22 22:01:41 +0000

    Added checklist and radioitems

--- app.py
+++ app.py
@@ -14,14 +14,75 @@ external_stylesheets = [dbc.themes.BOOTSTRAP]
 # Pass the stylesheet and meta_tag variables to the Dash app constructor
 app = Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=meta_tags)
 
-# Wrap the layout in a Bootstrap container
-app.layout = dbc.Container([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
-    # Add the HTML layout components in here
-])
+region_checklist = html.Div(
+    [
+        dbc.Label("Select the region"),
+        dbc.RadioItems(
+            options=[
+                {"label": "England", "value": "england"},
+                {"label": "Wales", "value": "wales"}
+            ],
+            value=[],
+            id="region-checklist",
+        ),
+    ]
+)
 
+year_checklist = html.Div(
+    [
+        dbc.Label("Select the year"),
+        dbc.RadioItems(
+            options=[
+                {"label": "2021", "value": "2021"},
+                {"label": "2022", "value": "2022"},
+                {"label": "2023", "value": "2023"}
+            ],
+            value=[],
+            id="year-checklist",
+        ),
+    ]
+)
+
+occupation_checklist = html.Div(
+    [
+        dbc.Label("Select the occupation type"),
+        dbc.Checklist(
+            options=[
+                {"label": "1: managers, directors and senior officials", "value": "Type 1"},
+                {"label": "2: professional occupations", "value": "Type 2"},
+                {"label": "3: associate prof & tech occupations", "value": "Type 3"}, 
+                {"label": "4: administrative and secretarial occupations", "value": "Type 4"},
+                {"label": "5: skilled trades occupations", "value": "Type 5"},
+                {"label": "6: caring, leisure and other service occupations", "value": "Type 6"},
+                {"label": "7: sales and customer service occupations", "value": "Type 7"},
+                {"label": "8: process, plant and machine operatives", "value": "Type 8"},
+                {"label": "9: elementary occupations", "value": "Type 9"},
+            ],
+            value=[],
+            id="occupation-checklist",
+        ),
+    ]
+)
+
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(html.H1('Comparative Employment Analysis Tool'))
+    ]),
+
+    dbc.Row([
+        dbc.Col(html.H2('Selection filters'))
+    ]),
+
+    dbc.Row([
+        dbc.Col(region_checklist, width=3),
+        dbc.Col(year_checklist, width={"size": 3, "offset": 1}),
+        dbc.Col(occupation_checklist, width={"size": 3, "offset": 1})
+    ]),
+
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='graph-content'))
+    ])
+])
 
 @callback(
     Output('graph-content', 'figure'),
