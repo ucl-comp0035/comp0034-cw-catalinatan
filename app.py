993bc016d3d844bf244b9c88e49b1858be13b823
catalinatan <zcemcat@ucl.ac.uk>
2025-01-22 15:24:04 +0000

    Added CSS bootstrap components

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 from dash import Dash, html, dcc, callback, Output, Input
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+# Variable that defines the meta tag for the viewport
+meta_tags = [
+    {"name": "viewport", "content": "width=device-width, initial-scale=1"},
+]
 
-app = Dash()
+# Variable that contains the external_stylesheet to use, in this case Bootstrap styling from dash bootstrap components (dbc)
+external_stylesheets = [dbc.themes.BOOTSTRAP]
 
-app.layout = [
+# Pass the stylesheet and meta_tag variables to the Dash app constructor
+app = Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=meta_tags)
+
+# Wrap the layout in a Bootstrap container
+app.layout = dbc.Container([
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
     dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
     dcc.Graph(id='graph-content')
-]
+    # Add the HTML layout components in here
+])
+
 
 @callback(
     Output('graph-content', 'figure'),
